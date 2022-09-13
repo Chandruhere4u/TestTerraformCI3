2ba81ef51118c05f84421f52095457cb9a6aaf44
Chandruhere4u <chandruhere4u@gmail.com>
2022-09-12 23:11:02 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,26 @@
-provider "aws" {
-    access_key = "${var.aws_access_key}"
-    secret_key = "${var.aws_secret_key}"
-    region = "${var.region}"
-}
+# provider "aws" {
+#     access_key = "${var.aws_access_key}"
+#     secret_key = "${var.aws_secret_key}"
+#     region = "${var.region}"
+# }
 
-module "s3" {
-    source = "<path-to-S3-folder>"
-    #bucket name should be unique
-    bucket_name = "<Bucket-name>"       
+provider "aws" {
+  region = "us-east-1"
+  assume_role {
+    role_arn = "arn:aws:iam::350978175498:role/Test-Git-ECS-Role"
+    session_name = "TestTerrformAssumerole"
+  }
+
+# module "s3" {
+#     source = "<path-to-S3-folder>"
+#     #bucket name should be unique
+#     bucket_name = "<Bucket-name>"       
+# }
+    
+resource "aws_instance" "TestInsanceAssumeRole" {
+  ami = "ami-01147599e39cb5802"
+  instance_type = "t2.micro"
+  tags = {
+    name="TestInstance2"
+  }
 }
